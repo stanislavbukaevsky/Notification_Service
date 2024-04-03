db12342b6fa95a828a79cf82871a3425576daaa8
Stanislav <stanislavi18061989@gmail.com>
2024-04-03 16:13:23 +0300

    1-Тестовый коммит

--- src/main/java/com/github/stanislavbukaevsky/notificationservice/NotificationServiceApplication.java
+++ src/main/java/com/github/stanislavbukaevsky/notificationservice/NotificationServiceApplication.java
@@ -8,6 +8,7 @@ public class NotificationServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(NotificationServiceApplication.class, args);
+		System.out.println();
 	}
 
 }
